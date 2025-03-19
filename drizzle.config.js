06268b275700f419085248e680bd5cd8babf3831
y-a-s-h-S <yashsingh2903@gmail.com>
2025-03-19 09:35:18 +0530

    some changes in footer

--- drizzle.config.js
+++ drizzle.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: './config/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url:'postgresql://accounts:1svCfPA9Kpae@ep-square-math-a5itkttz.us-east-2.aws.neon.tech/ai-room-redesign?sslmode=require',
+    url:process.env.DBCRED || '',
   },
 });
